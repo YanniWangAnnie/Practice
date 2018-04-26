29148f96085ea89cf97ba382c55a6c0cd22ab55b
YanniWangAnnie <yanniwangannie@gmail.com>
2018-04-25 23:02:54 -0700

    sort colors

--- Practice/Practice/code/SortColors.cs
+++ Practice/Practice/code/SortColors.cs
@@ -3,9 +3,42 @@ namespace Practice.Code
 {
     public class SortColors
     {
-        public SortColors()
+        public void SortCol(int[] nums)
         {
-        }
+            int begin = 0;
+            int cur = 0;
 
+
+            if (nums == null || nums.Length <= 1)
+            {
+                return;
+            }
+
+            int end = nums.Length - 1;
+
+            while (cur <= end)
+            {
+                if (nums[cur] == 0)
+                {
+                    nums[cur] = nums[begin];
+                    nums[begin] = 0;
+                    begin++;
+                    cur++;
+
+                }
+                else if (nums[cur] == 1)
+                {
+                    cur++;
+                }
+                else
+                {
+                    nums[cur] = nums[end];
+                    nums[end] = 2;
+                    end--;
+                }
+            }
+
+        }
     }
 }
+
--- Practice/PracticeTests/Tests/TestSortColors.cs
+++ Practice/PracticeTests/Tests/TestSortColors.cs
@@ -7,11 +7,38 @@ namespace PracticeTests.Tests
     public class TestSortColors
     {
         [TestMethod]
-        public void Testxxxx()
+        public void TestFunction()
         {
-            
+            SortColors sc = new SortColors();
+            int[] nums = new int[6] { 2, 0, 2, 1, 1, 0 };
+
+            sc.SortCol(nums);
+
+            int[] expected = new int[6] { 0, 0, 1, 1, 2, 2 };
+            Assert.AreEqual(expected.Length, nums.Length);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Assert.AreEqual(expected[i], nums[i]);
+            }
         }
 
-        public void Test
+        [TestMethod]
+        public void Testnull(){
+            SortColors sc = new SortColors();
+            int[] nums = null;
+
+            sc.SortCol(nums);
+
+            Assert.AreEqual(null, nums);
+        }
+
+       [TestMethod]
+        public void TestZeroLen(){
+            SortColors sc = new SortColors();
+            int[] nums = new int[0];
+            sc.SortCol(nums);
+
+            Assert.AreEqual(0, nums.Length);
+        }
     }
 }
